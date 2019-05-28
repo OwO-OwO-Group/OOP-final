854046842ade93bca3b59fb78112c022a02e88b3
r888800009 <r888800009@gmail.com>
2019-05-28 16:02:12 +0800

    Add LMS Ship

---
+++ LargeShip.java
@@ -0,0 +1,13 @@
+/*
+ * 10627108 陳文捷 10612150 林詠翔 資訊二甲
+ * UTF-8
+ */
+package hw2;
+
+public class LargeShip extends DotCom {
+    public LargeShip(String nameStr) {
+      super(nameStr, 4);
+      super.setHitShout("SMAcannot find symbolSH!");
+      super.setHitMoveRange(2);
+    }
+}
